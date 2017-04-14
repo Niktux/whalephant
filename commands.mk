9bbabdaa4806aec741da7d80754d36fc240d37f2
Niktux <nico.ln@gmail.com>
2017-04-14 22:45:12 +0200

    chore: self documented make

--- commands.mk
+++ commands.mk
@@ -4,6 +4,8 @@
 CONSOLE_IMAGE_NAME=whalephant/commands
 CONSOLE_CONTAINER_SOURCE_PATH=/usr/src/whalephant
 
+.DEFAULT_GOAL := help
+
 console_exec = docker run -it --rm --name whalephant_console \
 	                 -v ${HOST_SOURCE_PATH}:${CONSOLE_CONTAINER_SOURCE_PATH} \
 	                 -w ${CONSOLE_CONTAINER_SOURCE_PATH} \
@@ -11,31 +13,36 @@ console_exec = docker run -it --rm --name whalephant_console \
 	                 ${CONSOLE_IMAGE_NAME} \
 	                 ./console $1 $(CLI_ARGS)
 
-gen: generate
+gen: generate ## Generate and display Dockerfile 
 	cat Dockerfile
 
-test: generate only-test
+test: generate only-test ## Generate, build and test
 
-only-test:
+only-test: ## Build container from generated Dockerfile and display php info
 	docker build -t whalephant-generated .
 	docker run -it --rm --name whalephant_test whalephant-generated php -i
 
-connect:
+connect: ## Build container and run bash
 	docker run -it --rm --name whalephant_test whalephant-generated /bin/bash
 
-check:
+check: ## Build container and check php modules
 	docker run -it --rm --name whalephant_test whalephant-generated php -r 'phpinfo(INFO_MODULES);'
 
-generate: create-cli-image
+generate: create-cli-image ## Generate Dockerfile from Whalephant file
 	$(call console_exec, generate)
 	
-help: create-cli-image
+cli-help: create-cli-image ## Help Whalephant
 	$(call console_exec, )
 
-create-cli-image:
+create-cli-image: ## Create Whalephant image
 	docker build -q -t ${CONSOLE_IMAGE_NAME} docker/images/cli/
 
-clean-cli-image:
+clean-cli-image: ## Delete Whalephant image
 	docker rmi ${CONSOLE_IMAGE_NAME}
 
 .PHONY: generate help create-cli-image clean-cli-image
+
+.PHONY: help
+
+help: ## This help.
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
