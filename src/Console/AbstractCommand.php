942e754fdd6d5df297b3144e956e8913d32fa674
Niktux <nico.ln@gmail.com>
2017-04-15 16:09:25 +0200

    fix

--- src/Console/AbstractCommand.php
+++ src/Console/AbstractCommand.php
@@ -5,7 +5,6 @@ namespace Whalephant\Console;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Whalephant\Application;
 
 abstract class AbstractCommand extends Command
 {
