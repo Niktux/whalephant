974b258dd95d6ea8244a3158e13a2ecc5784d26e
Niktux <nico.ln@gmail.com>
2017-04-15 16:12:18 +0200

    chore

--- commands.mk
+++ commands.mk
@@ -45,4 +45,4 @@ clean-cli-image: ## Delete Whalephant image
 .PHONY: help generate help create-cli-image clean-cli-image
 
 help: ## This help.
-	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
