640ff36cacc358d9d4e2b3c66a4b586bfcad923d
Niktux <nico.ln@gmail.com>
2017-04-16 00:32:10 +0200

    chore: bump to 0.2.1

--- src/Application.php
+++ src/Application.php
@@ -10,7 +10,7 @@ class Application extends \Onyx\Application
 {
     const
         WHALEPHANT_FILENAME = 'whalephant.yml',
-        VERSION = '0.2.0';
+        VERSION = '0.2.1';
     
     protected function registerProviders(): void
     {
