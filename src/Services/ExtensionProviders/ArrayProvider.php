20f8170aa5ff7dfb84d4d1708138f2cfe8a7c94e
Niktux <nico.ln@gmail.com>
2017-05-27 02:11:57 +0200

    feat(extensions): add calendar

--- src/Services/ExtensionProviders/ArrayProvider.php
+++ src/Services/ExtensionProviders/ArrayProvider.php
@@ -13,22 +13,24 @@ use Whalephant\Model\Extensions\Memcached;
 use Whalephant\Model\Extensions\GD;
 use Whalephant\Model\Extensions\MySQL;
 use Whalephant\Model\Extensions\Postgresql;
+use Whalephant\Model\Extensions\Calendar;
 
 class ArrayProvider implements ExtensionProvider
 {
     private
         $extensions;
-    
+
     public function __construct()
     {
         $this->extensions = [];
         $this->init();
     }
-    
+
     private function init(): void
     {
         $this
             ->register(new Xdebug())
+            ->register(new Calendar())
             ->register(new Amqp())
             ->register(new Zip())
             ->register(new Meminfo())
@@ -39,35 +41,35 @@ class ArrayProvider implements ExtensionProvider
             ->register(new GD())
         ;
     }
-    
+
     private function register(Extension $e): self
     {
         $this->extensions[strtolower($e->getName())] = $e;
-        
+
         return $this;
     }
-    
+
     public function exists(string $name): bool
     {
         return isset($this->extensions[strtolower($name)]);
     }
-    
+
     public function get(string $name): ?Extension
     {
         if(! $this->exists($name))
         {
             return null;
         }
-        
+
         return $this->extensions[$name];
     }
-    
+
     public function listNames(): iterable
     {
         $names = array_keys($this->extensions);
-        
+
         sort($names);
-        
+
         return $names;
     }
 }
